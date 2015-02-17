83760f15096e839457ce8f3c3ec279c25b9963a5
carole bacon <carolelynnbacon@gmail.com>
2015-02-17 13:16:33 -0800

    C# Software Sales

---
+++ Software Sales/softwaresales.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Software_Sales
+{
+    partial class softwareSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.numberOfPackagesTextBox = new System.Windows.Forms.TextBox();
+            this.discountLabel = new System.Windows.Forms.Label();
+            this.subTotalPriceTextBox = new System.Windows.Forms.TextBox();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalPriceTextBox = new System.Windows.Forms.TextBox();
+            this.softwareSalesLabel = new System.Windows.Forms.Label();
+            this.calculateTotalButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountLabel.Location = new System.Drawing.Point(45, 75);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(303, 20);
+            this.amountLabel.TabIndex = 0;
+            this.amountLabel.Text = "Enter the amount of packages purchased";
+            // 
+            // numberOfPackagesTextBox
+            // 
+            this.numberOfPackagesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberOfPackagesTextBox.Location = new System.Drawing.Point(509, 72);
+            this.numberOfPackagesTextBox.Name = "numberOfPackagesTextBox";
+            this.numberOfPackagesTextBox.Size = new System.Drawing.Size(100, 26);
+            this.numberOfPackagesTextBox.TabIndex = 1;
+            // 
+            // discountLabel
+            // 
+            this.discountLabel.AutoSize = true;
+            this.discountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discountLabel.Location = new System.Drawing.Point(45, 131);
+            this.discountLabel.Name = "discountLabel";
+            this.discountLabel.Size = new System.Drawing.Size(122, 20);
+            this.discountLabel.TabIndex = 7;
+            this.discountLabel.Text = "Your discount is";
+            // 
+            // subTotalPriceTextBox
+            // 
+            this.subTotalPriceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalPriceTextBox.Location = new System.Drawing.Point(509, 128);
+            this.subTotalPriceTextBox.Name = "subTotalPriceTextBox";
+            this.subTotalPriceTextBox.Size = new System.Drawing.Size(100, 26);
+            this.subTotalPriceTextBox.TabIndex = 8;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(45, 191);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(93, 20);
+            this.totalLabel.TabIndex = 9;
+            this.totalLabel.Text = "Your total is";
+            // 
+            // totalPriceTextBox
+            // 
+            this.totalPriceTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalPriceTextBox.Location = new System.Drawing.Point(509, 185);
+            this.totalPriceTextBox.Name = "totalPriceTextBox";
+            this.totalPriceTextBox.Size = new System.Drawing.Size(100, 26);
+            this.totalPriceTextBox.TabIndex = 10;
+            // 
+            // softwareSalesLabel
+            // 
+            this.softwareSalesLabel.AutoSize = true;
+            this.softwareSalesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.softwareSalesLabel.Location = new System.Drawing.Point(12, 9);
+            this.softwareSalesLabel.Name = "softwareSalesLabel";
+            this.softwareSalesLabel.Size = new System.Drawing.Size(147, 24);
+            this.softwareSalesLabel.TabIndex = 11;
+            this.softwareSalesLabel.Text = "Software Sales";
+            // 
+            // calculateTotalButton
+            // 
+            this.calculateTotalButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateTotalButton.Location = new System.Drawing.Point(99, 263);
+            this.calculateTotalButton.Name = "calculateTotalButton";
+            this.calculateTotalButton.Size = new System.Drawing.Size(89, 38);
+            this.calculateTotalButton.TabIndex = 2;
+            this.calculateTotalButton.Text = "Calculate";
+            this.calculateTotalButton.UseVisualStyleBackColor = true;
+            this.calculateTotalButton.Click += new System.EventHandler(this.calculateTotalButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(298, 263);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(84, 38);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(493, 263);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(82, 38);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // softwareSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 344);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateTotalButton);
+            this.Controls.Add(this.softwareSalesLabel);
+            this.Controls.Add(this.totalPriceTextBox);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.subTotalPriceTextBox);
+            this.Controls.Add(this.discountLabel);
+            this.Controls.Add(this.numberOfPackagesTextBox);
+            this.Controls.Add(this.amountLabel);
+            this.Name = "softwareSales";
+            this.Text = "SoftwareSales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.TextBox numberOfPackagesTextBox;
+        private System.Windows.Forms.Label discountLabel;
+        private System.Windows.Forms.TextBox subTotalPriceTextBox;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox totalPriceTextBox;
+        private System.Windows.Forms.Label softwareSalesLabel;
+        private System.Windows.Forms.Button calculateTotalButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Software Sales/softwaresales.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Sales
+{
+    public partial class softwareSales : Form
+    {
+        public softwareSales()
+        {
+            InitializeComponent();
+        }
+
+        // local variables
+
+        decimal numberOfPackages, discountAmount, subTotalPrice, totalPrice;
+
+        const decimal packageAmount = 99m;
+        const decimal discountTwenty = 0.2m;
+        const decimal discountThirty = 0.3m;
+        const decimal discountForty = 0.4m;
+        const decimal discountFifty = 0.5m;
+
+
+        private void calculateTotalButton_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(numberOfPackagesTextBox.Text, out numberOfPackages) && (numberOfPackages >= 1 && numberOfPackages <= 1000))
+            {
+                if (numberOfPackages >= 10 && numberOfPackages <= 19)
+                {
+                    discountAmount = discountTwenty;
+                }
+                else if (numberOfPackages >= 20 && numberOfPackages <= 49)
+                {
+                    discountAmount = discountThirty;
+                }
+                else if (numberOfPackages >= 50 && numberOfPackages <= 99)
+                {
+                    discountAmount = discountForty;
+                }
+                else if (numberOfPackages >= 100 && numberOfPackages <= 1000)
+                {
+                    discountAmount = discountFifty;
+                }
+
+                subTotalPrice = packageAmount * numberOfPackages * discountAmount;
+                totalPrice = packageAmount * numberOfPackages - subTotalPrice;
+                subTotalPriceTextBox.Text = subTotalPrice.ToString("c");
+                totalPriceTextBox.Text = totalPrice.ToString("c");
+                numberOfPackagesTextBox.Focus();
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter whole number between 1 and 1,000");
+                numberOfPackagesTextBox.Focus();
+                numberOfPackagesTextBox.SelectAll();
+
+                numberOfPackagesTextBox.Clear();
+                totalPriceTextBox.Clear();
+            }
+
+    }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numberOfPackagesTextBox.Clear();
+            subTotalPriceTextBox.Clear();
+            totalPriceTextBox.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // closes the form
+            this.Close();
+        }
+
+        
+    }
+}
